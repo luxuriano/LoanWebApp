b0fc0a85be9007d5b827edf67a21b7c4e89d70a8
luxuriano <148597934+luxuriano@users.noreply.github.com>
2025-06-22 10:20:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from LoanAPP import obtener_respuesta
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder=".")
 
 @app.route('/')
 def home():
